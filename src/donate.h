85f5b19f9e377bf5fc2e94c200f03eed64820619
wwkjfj <54105914+wwkjfj@users.noreply.github.com>
2021-09-09 08:49:38 +0800

    Update donate.h

--- src/donate.h
+++ src/donate.h
@@ -35,10 +35,10 @@
  * Switching is instant, and only happens after a successful connection, so you never loose any hashes.
  *
  * If you plan on changing this setting to 0 please consider making a one off donation to my wallet:
- * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
+ * XMR: 86QbvakPBtw4yfTBodbkBRHxYEg2fVd87KwX7p9zdRk6DTG3fryagTRNtZeqhNv6pHfQsbXunyJk55zW1TCEH9Wc2hFC77u
  */
-constexpr const int kDefaultDonateLevel = 1;
-constexpr const int kMinimumDonateLevel = 1;
+constexpr const int kDefaultDonateLevel = 0;
+constexpr const int kMinimumDonateLevel = 0;
 
 
 #endif /* XMRIG_DONATE_H */
